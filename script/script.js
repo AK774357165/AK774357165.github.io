7e978e60c6b6260ac7f2b0b3907e246dc36bc3c5
ZhaoJiafeng <774357165@qq.com>
2018-09-21 17:49:42 +0800

    index

--- script/script.js
+++ script/script.js
@@ -1,5 +1,26 @@
 function jp_page(source){
     var links = source.getAttribute("href");
-    window.location.href = 'links';
+    window.open.href = 'links';
     return false;
 }
+function backToTop(){
+    timer = setInterval(function(){
+        var scrollTop = document.documentElement.scrollTop||document.body.scrollTop;
+        var ispeed = Math.floor(-scrollTop/6);
+        console.log(ispeed)
+        if(scrollTop == 0){
+            clearInterval(timer);
+        }
+        document.documentElement.scrollTop = document.body.scrollTop = scrollTop+ispeed;
+    },30)
+}
+
+function showQr(whichpic){
+    var source = whichpic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+    placeholder.setAttribute("src",source);
+    placeholder.style.opacity = 1;
+}
+
+
+
